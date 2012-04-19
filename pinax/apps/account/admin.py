9b19d05f4cc1cbf1c807c0c272c77aa922a54de5
Jun Du <duj@bitauto.com>
2012-04-19 18:22:20 +0800

    changed for chinese supportting in username.

--- pinax/apps/account/admin.py
+++ pinax/apps/account/admin.py
@@ -7,5 +7,30 @@ class PasswordResetAdmin(admin.ModelAdmin):
     list_display = ["user", "temp_key", "timestamp", "reset"]
 
 
+class MyUserCreationForm(UserCreationForm):
+    username = forms.RegexField(
+            label = 'Username',
+            max_length = 30,
+            regex = u"^[\w-]+|[\u4e00-\u9fa5]+$",
+            help_text = 'Required. 30 characters or fewer. Alphanumeric characters only (letters, digits, hyphens and underscores).',
+            error_message = 'This value must contain only letters, numbers, hyphens and underscores.')
+
+class MyUserChangeForm(UserChangeForm):
+    username = forms.RegexField(
+        label = 'Username',
+        max_length = 30,
+        regex = u'^[\w-]+|[\u4e00-\u9fa5]+$',
+        help_text = 'Required. 30 characters or fewer. Alphanumeric characters only (letters, digits, hyphens and underscores).',
+        error_message = 'This value must contain only letters, numbers, hyphens and underscores.')
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+
+
+admin.site.unregister(User)
+admin.site.register(User,MyUserAdmin)
+
+
 admin.site.register(Account)
 admin.site.register(PasswordReset, PasswordResetAdmin)
